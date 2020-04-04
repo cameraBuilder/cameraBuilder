078608c23b9f66be038209f25c8171d284438d86
gatco_chiraz <thecode371@hotmail.com>
2020-04-04 11:48:27 +0300

    - add home and splash page

--- app/src/main/java/com/hackathon/camerabuilder/ui/MainActivity.java
+++ app/src/main/java/com/hackathon/camerabuilder/ui/MainActivity.java
@@ -3,7 +3,6 @@ package com.hackathon.camerabuilder.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
 import com.hackathon.camerabuilder.BaseActivity;
 import com.hackathon.camerabuilder.R;
 import com.hackathon.camerabuilder.databinding.ActivityMainBinding;
@@ -23,5 +22,11 @@ public class MainActivity extends BaseActivity<ActivityMainBinding> {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mViewDataBinding.tvUsername.setText(repository.getUserInfo().getUsername());
+        mViewDataBinding.btnLogout.setOnClickListener(view -> {
+            repository.logout();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finishAffinity();
+        });
     }
 }
